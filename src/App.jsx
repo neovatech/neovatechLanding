aa5408db8bcfb9c0a8c8d73febd3d7f2efeac8e8
RIMON DEBNATH <68506201+Rimonok12@users.noreply.github.com>
2025-04-20 16:29:49 +0000

    Changes from rimon with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>hello NeovaTech</h1>
       <h2>hii from shachi</h2>
-      <h2>hii from rimon</h2>
+      <h2 className="abs">hii from rimon</h2>
     </>
   );
 }
