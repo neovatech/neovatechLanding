5106f9a77bc0a1e91fd1aff0aa4c84f495707bb7
Neova Tech <techneova@gmail.com>
2025-04-20 17:28:26 +0000

    by ishan

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
 
       <header className="hero-section">
         <h1>Welcome to NeovaTech</h1>
+        <h2>heyhihihi</h2>
         <p>Your reliable partner in full-stack & cloud solutions.</p>
         <div className="cta-buttons">
           <button className="primary-btn">Get Started</button>
